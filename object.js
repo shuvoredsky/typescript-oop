6592bc1f17c763d8665763e0588bbb1c40b9b6b9
shuvoredsky <kumarshuvo265@gmail.com>
2025-10-16 20:01:48 +0600

    explore typescript class method

--- object.js
+++ object.js
@@ -1,4 +1,6 @@
+// -----------------------------------------------------------
 // Simple Object
+// -----------------------------------------------------------
 // let book: { title: string; pages: number } = {
 //   title: "Byomkesh Bokshi",
 //   pages: 30,
@@ -6,18 +8,48 @@
 // console.log(`Book Name: ${book.title}, page: ${book.pages}`);
 // book.pages = 550;
 // console.log(`update: ${book.title} pages: ${book.pages}`);
+// -----------------------------------------------------------
 // Step 2: Class
-var Student = /** @class */ (function () {
-    function Student(n, g) {
-        this.name = n;
-        this.grade = g;
+// -----------------------------------------------------------
+// class Student {
+//   name: string;
+//   grade: number;
+//   constructor(n: string, g: number) {
+//     this.name = n;
+//     this.grade = g;
+//   }
+//   greet(): string {
+//     return `Hello, Ami ${this.name}, class ${this.grade}!`;
+//   }
+// }
+// let shuvo = new Student("Shuvo", 6);
+// let sonia = new Student("sonia", 7);
+// console.log(shuvo.greet());
+// console.log(sonia.greet());
+// -----------------------------------------------------------
+// step 3 Access Modifiers  = public, private, protected
+// -----------------------------------------------------------
+var BankAccount = /** @class */ (function () {
+    function BankAccount() {
+        this.balance = 1000;
+        this.pin = 1234;
+        this.accountNumber = "ACC001";
     }
-    Student.prototype.greet = function () {
-        return "Hello, Ami ".concat(this.name, ", class ").concat(this.grade, "!");
+    BankAccount.prototype.getBalance = function () {
+        return this.balance;
     };
-    return Student;
+    BankAccount.prototype.checkPin = function (enteredPin) {
+        return enteredPin === this.pin;
+    };
+    BankAccount.prototype.withdraw = function (amount, pin) {
+        if (this.checkPin(pin)) {
+            this.balance -= amount;
+            return "Withdraw: ".concat(amount, ", Due: ").concat(this.balance);
+        }
+        return "Wrong Pin!";
+    };
+    return BankAccount;
 }());
-var shuvo = new Student("Shuvo", 6);
-var sonia = new Student("sonia", 7);
-console.log(shuvo.greet());
-console.log(sonia.greet());
+var myAccount = new BankAccount();
+console.log(myAccount.getBalance());
+console.log(myAccount.withdraw(200, 1234));
--- object.ts
+++ object.ts
@@ -1,4 +1,6 @@
+// -----------------------------------------------------------
 // Simple Object
+// -----------------------------------------------------------
 // let book: { title: string; pages: number } = {
 //   title: "Byomkesh Bokshi",
 //   pages: 30,
@@ -9,23 +11,54 @@
 // book.pages = 550;
 // console.log(`update: ${book.title} pages: ${book.pages}`);
 
+// -----------------------------------------------------------
 // Step 2: Class
+// -----------------------------------------------------------
 
-class Student {
-  name: string;
-  grade: number;
+// class Student {
+//   name: string;
+//   grade: number;
 
-  constructor(n: string, g: number) {
-    this.name = n;
-    this.grade = g;
+//   constructor(n: string, g: number) {
+//     this.name = n;
+//     this.grade = g;
+//   }
+//   greet(): string {
+//     return `Hello, Ami ${this.name}, class ${this.grade}!`;
+//   }
+// }
+
+// let shuvo = new Student("Shuvo", 6);
+// let sonia = new Student("sonia", 7);
+
+// console.log(shuvo.greet());
+// console.log(sonia.greet());
+
+// -----------------------------------------------------------
+// step 3 Access Modifiers  = public, private, protected
+// -----------------------------------------------------------
+
+class BankAccount {
+  public balance: number = 1000;
+  private pin: number = 1234;
+  protected accountNumber: string = "ACC001";
+
+  public getBalance(): number {
+    return this.balance;
   }
-  greet(): string {
-    return `Hello, Ami ${this.name}, class ${this.grade}!`;
+  private checkPin(enteredPin: number): boolean {
+    return enteredPin === this.pin;
+  }
+
+  public withdraw(amount: number, pin: number): string {
+    if (this.checkPin(pin)) {
+      this.balance -= amount;
+      return `Withdraw: ${amount}, Due: ${this.balance}`;
+    }
+    return "Wrong Pin!";
   }
 }
 
-let shuvo = new Student("Shuvo", 6);
-let sonia = new Student("sonia", 7);
-
-console.log(shuvo.greet());
-console.log(sonia.greet());
+let myAccount = new BankAccount();
+console.log(myAccount.getBalance());
+console.log(myAccount.withdraw(200, 1234));

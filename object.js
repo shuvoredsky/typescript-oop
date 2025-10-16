2cbe623ff617c715b975dfb121bc40c3862018d3
shuvoredsky <kumarshuvo265@gmail.com>
2025-10-16 17:33:13 +0600

    explore class

--- object.js
+++ object.js
@@ -1,6 +1,23 @@
 // Simple Object
-var book = {
-    title: "Byomkesh Bokshi",
-    pages: 30,
-};
-console.log("Book Name: ".concat(book.title, ", page: ").concat(book.pages));
+// let book: { title: string; pages: number } = {
+//   title: "Byomkesh Bokshi",
+//   pages: 30,
+// };
+// console.log(`Book Name: ${book.title}, page: ${book.pages}`);
+// book.pages = 550;
+// console.log(`update: ${book.title} pages: ${book.pages}`);
+// Step 2: Class
+var Student = /** @class */ (function () {
+    function Student(n, g) {
+        this.name = n;
+        this.grade = g;
+    }
+    Student.prototype.greet = function () {
+        return "Hello, Ami ".concat(this.name, ", class ").concat(this.grade, "!");
+    };
+    return Student;
+}());
+var shuvo = new Student("Shuvo", 6);
+var sonia = new Student("sonia", 7);
+console.log(shuvo.greet());
+console.log(sonia.greet());
--- object.ts
+++ object.ts
@@ -1,10 +1,31 @@
 // Simple Object
-let book: { title: string; pages: number } = {
-  title: "Byomkesh Bokshi",
-  pages: 30,
-};
+// let book: { title: string; pages: number } = {
+//   title: "Byomkesh Bokshi",
+//   pages: 30,
+// };
 
-console.log(`Book Name: ${book.title}, page: ${book.pages}`);
+// console.log(`Book Name: ${book.title}, page: ${book.pages}`);
 
-book.pages = 550;
-console.log(`update: ${book.title} pages`);
+// book.pages = 550;
+// console.log(`update: ${book.title} pages: ${book.pages}`);
+
+// Step 2: Class
+
+class Student {
+  name: string;
+  grade: number;
+
+  constructor(n: string, g: number) {
+    this.name = n;
+    this.grade = g;
+  }
+  greet(): string {
+    return `Hello, Ami ${this.name}, class ${this.grade}!`;
+  }
+}
+
+let shuvo = new Student("Shuvo", 6);
+let sonia = new Student("sonia", 7);
+
+console.log(shuvo.greet());
+console.log(sonia.greet());
